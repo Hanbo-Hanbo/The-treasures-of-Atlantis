a6ce211fdd2e9ce3170ff84c119bafcc33399972
Hanbo-Hanbo <bobibobibobi1224@gmail.com>
2026-02-07 00:15:57 +0000

    fix5

--- app.js
+++ app.js
@@ -6,11 +6,8 @@ const io = require("socket.io")(http);
 app.use(express.static("public"));
 
 let treasures = [];
-let starsCount = 0;
-let activatedHistory = []; // 存储已被激活的宝藏及其颜色
-
 function initMap() {
-    // 每次生成10个宝藏，去掉炸弹
+    // 10 treasures per round, no bombs
     treasures = Array.from({length: 10}, () => ({
         x: Math.random(), 
         y: Math.random(), 
@@ -21,26 +18,21 @@ function initMap() {
 initMap();
 
 io.on("connection", (socket) => {
-    socket.emit("init-game", { treasures, starsCount });
+    socket.emit("init-game", { treasures });
 
     socket.on("refresh-location", (data) => {
-        // 广播新位置给所有人看到
-        socket.broadcast.emit("player-pulse", data);
+        socket.broadcast.emit("player-pulse", data); // Show other players
 
-        // 检测碰撞
         treasures.forEach(t => {
+            // Collision detection in normalized space
             if (!t.found && Math.hypot(data.xpos - t.x, data.ypos - t.y) < 0.08) {
                 t.found = true;
                 t.foundByColor = { r: data.userR, g: data.userG, b: data.userB };
-                io.emit("treasure-activated", { treasures, starsCount });
+                io.emit("treasure-activated", { treasures });
             }
         });
 
-        if (treasures.every(t => t.found)) {
-            starsCount++;
-            initMap();
-            io.emit("level-up", { treasures, starsCount });
-        }
+        if (treasures.every(t => t.found)) initMap();
     });
 });
 
